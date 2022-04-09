50be7fa256b74e52c3034f2c667f6bc91cd822c0
Ryhann <rayhancoding1603@gmail.com>
2022-04-09 12:36:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: const Text('Ini Apaan Dah Kok Keren?')),
+      body: Center(
+          child: Container(
+              color: Colors.lightBlue,
+              width: 150,
+              height: 150,
+              child: Text(
+                'Moto Hape Didalem Hape anjay gurinjay tak kunjay',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w800),
+                textAlign: TextAlign.center,
+                maxLines: 2, // max line
+                overflow: TextOverflow.clip, // text overflow
+              ))),
+    ));
+  }
+}
