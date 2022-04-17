013f8ca8a3f1fb53892b2f40e9de8804c7cfa2b3
Ryhann <rayhancoding1603@gmail.com>
2022-04-17 13:17:03 +0700

    Add Stateless & Statefull Widget ( Membuat simple Count )

--- lib/main.dart
+++ lib/main.dart
@@ -67,22 +67,76 @@ void main() {
 // Margin Padding
 // Decoration - gradient
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//             appBar: AppBar(title: Text("Latihan Container")),
+//             body: Container(
+//                 color: Colors.red,
+//                 margin: EdgeInsets.all(10),
+//                 padding: EdgeInsets.fromLTRB(10, 30, 10, 30),
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                       gradient: LinearGradient(
+//                           begin: Alignment.topLeft,
+//                           end: Alignment.bottomRight,
+//                           colors: <Color>[Colors.amber, Colors.blue])),
+//                 ))));
+//   }
+// }
+
+// Stateless & Statefull Widget
+// Stateless = State yang bersifat static atau tidak berubah2
+// Statefull = State yang bersifat dinamis atau bisa diubah2
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int number = 0;
+
+  void tekanTombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  void tombolKurang() {
+    setState(() {
+      number = number - 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: Text("Latihan Container")),
-            body: Container(
-                color: Colors.red,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.fromLTRB(10, 30, 10, 30),
-                child: Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: <Color>[Colors.amber, Colors.blue])),
-                ))));
+      appBar: AppBar(title: Text("Statefull Widget ( Dinamis App )")),
+      body: Container(
+        color: Color.fromARGB(255, 50, 175, 115),
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 20),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(number.toString(),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40 + number.toDouble())),
+          ElevatedButton(
+            child: Text("Tambah Bilangan"),
+            onPressed: tekanTombol,
+          ),
+          ElevatedButton(onPressed: tombolKurang, child: Text("Tombol Kurang"))
+        ]),
+      ),
+    ));
   }
 }
