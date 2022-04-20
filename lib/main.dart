acea452213edbc04bc0062f09de21375d5885575
= <=>
2022-04-21 03:41:08 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -192,4 +192,40 @@ void main() {
 //       )),
 //     ));
 //   }
-// }
+// }\
+
+// ---------------------------- Video 12 ( List View )
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+
+  _MyAppState() {
+    for (int i = 0; i < 16; i++) {
+      widgets.add(Text(
+        "Text Ke -" + i.toString(),
+        style: TextStyle(fontSize: 45),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan ListView"),
+        ),
+        body: ListView(
+          children: widgets,
+        ),
+      ),
+    );
+  }
+}
