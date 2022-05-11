5c4458c421bf79e5314f93ba81c69ea7bad20333
= <=>
2022-05-11 12:00:50 +0700

    add some lesson;

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,31 @@ void main() {
 // Basic dasar komponen dart ( MaterialApp, Scafold, appBar, Body, center, container )
 // Styling
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: const Text('Ini Apaan Dah Kok Keren?')),
-      body: Center(
-          child: Container(
-              color: Colors.lightBlue,
-              width: 150,
-              height: 150,
-              child: Text(
-                'Moto Hape Didalem Hape anjay gurinjay tak kunjay',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20,
-                  fontStyle: FontStyle.italic,
-                ),
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.clip, // text overflow
-              ))),
-    ));
-  }
-}
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//       appBar: AppBar(title: const Text('Ini Apaan Dah Kok Keren?')),
+//       body: Center(
+//           child: Container(
+//               color: Colors.lightBlue,
+//               width: 150,
+//               height: 150,
+//               child: Text(
+//                 'Moto Hape Didalem Hape anjay gurinjay tak kunjay',
+//                 style: TextStyle(
+//                   color: Colors.black,
+//                   fontWeight: FontWeight.w800,
+//                   fontSize: 20,
+//                   fontStyle: FontStyle.italic,
+//                 ),
+//                 textAlign: TextAlign.center,
+//                 overflow: TextOverflow.clip, // text overflow
+//               ))),
+//     ));
+//   }
+// }
+
+
