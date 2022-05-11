9d72c38657e3c314e26ed3a7494fe79919b9097f
= <=>
2022-05-11 11:58:11 +0700

    Add Stack Widget

--- lib/column.dart
+++ lib/column.dart
@@ -1,6 +1,7 @@
 // ----------------------------- Video 7 ( Column/Row Widget )
 // - Column
 // - Row
+// - Stack  = Lapisan widget
 
 import 'package:flutter/material.dart';
 
@@ -29,5 +30,144 @@ class MyApp extends StatelessWidget {
         ],
       ),
     ));
+  }
+}
+
+// Stack
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // background       = lapisan pertama
+  // listview text    = lapisan kedua
+  // button           = lapisan ketiga
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Stack Course"),
+          ),
+          body: Stack(
+            children: [
+              Column(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Row(children: [
+                      Flexible(child: Container(color: Colors.white)),
+                      Flexible(child: Container(color: Colors.black12))
+                    ]),
+                  ),
+                  // Flexible 1
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                              child: Container(
+                            color: Colors.black12,
+                          )),
+                          Flexible(
+                              child: Container(
+                            color: Colors.white,
+                          ))
+                        ],
+                      ))
+                  // Flexible 2
+                ],
+              ),
+              ListView(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(15),
+                        child: Text(
+                          "Ini adalah lapisan tengah dari stack",
+                          style: TextStyle(fontSize: 30),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState() {}
+                    },
+                    child: Text("My Button")),
+              )
+            ],
+          )),
+    );
   }
 }
